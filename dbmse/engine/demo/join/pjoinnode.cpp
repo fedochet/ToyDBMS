878ab86556ae5145d59a2ffe1cfe7f1ddfb97005
Roman Golyshev <anstreth@gmail.com>
2017-11-27 10:13:26 +0300

    remove std:: prefix from join node

--- dbmse/engine/demo/join/pjoinnode.cpp
+++ dbmse/engine/demo/join/pjoinnode.cpp
@@ -4,7 +4,7 @@
 // More details regarding the course can be found here: www.math.spbu.ru/user/chernishev/
 // CHANGELOG:
 // 0.4: no chance for efficiency competition, so, this year I reoriented task towards ideas:
-//      1) tried to remove C code, now we are using lots of std:: stuff
+//      1) tried to remove C code, now we are using lots of  stuff
 //      2) implemented support for multiple attributes in the DBMS
 //      3) code clean-up and restructurization
 // 0.3: added:
@@ -22,6 +22,8 @@
 
 #include "pjoinnode.h"
 
+using namespace std;
+
 PJoinNode::PJoinNode(PGetNextNode* left, PGetNextNode* right,
                      LAbstractNode* p): PGetNextNode(p, left, right) {
   pos = 0;
@@ -33,7 +35,7 @@ PJoinNode::~PJoinNode(){
   delete right;
 }
 
-std::vector<std::vector<Value>> PJoinNode::GetNext(){
+query_result PJoinNode::GetNext(){
   return data;
 }
 
@@ -42,17 +44,17 @@ void PJoinNode::Initialize(){
   PGetNextNode* r = (PGetNextNode*)right;
   LAbstractNode* lp = l->prototype;
   LAbstractNode* rp = r->prototype;
-  std::vector<std::vector<std::string>> ln = lp->fieldNames;
-  std::vector<std::vector<std::string>> rn = rp->fieldNames;
+  vector<vector<string>> ln = lp->fieldNames;
+  vector<vector<string>> rn = rp->fieldNames;
 
-  std::vector<std::vector<Value>> lres = l->GetNext();
-  std::vector<std::vector<Value>> rres = r->GetNext();
+  query_result lres = l->GetNext();
+  query_result rres = r->GetNext();
   LAbstractNode* p = prototype;
-  std::ptrdiff_t lpos, rpos;
+  ptrdiff_t lpos, rpos;
 
   for (int i = 0; i < lp->fieldNames.size(); i++){
-    std::ptrdiff_t lpos1 = std::find(ln[i].begin(), ln[i].end(), ((LJoinNode*)prototype)->offset1) - ln[i].begin();
-    std::ptrdiff_t lpos2 = std::find(ln[i].begin(), ln[i].end(), ((LJoinNode*)prototype)->offset2) - ln[i].begin();
+    ptrdiff_t lpos1 = find(ln[i].begin(), ln[i].end(), ((LJoinNode*)prototype)->offset1) - ln[i].begin();
+    ptrdiff_t lpos2 = find(ln[i].begin(), ln[i].end(), ((LJoinNode*)prototype)->offset2) - ln[i].begin();
 
     if(lpos1 <= ln.size() || lpos1 <= ln.size()){
       if (lpos1 < lpos2)
@@ -64,8 +66,8 @@ void PJoinNode::Initialize(){
   }
 
   for (int i = 0; i < rp->fieldNames.size(); i++){
-    std::ptrdiff_t rpos1 = std::find(rn[i].begin(), rn[i].end(), ((LJoinNode*)prototype)->offset1) - rn[i].begin();
-    std::ptrdiff_t rpos2 = std::find(rn[i].begin(), rn[i].end(), ((LJoinNode*)prototype)->offset2) - rn[i].begin();
+    ptrdiff_t rpos1 = find(rn[i].begin(), rn[i].end(), ((LJoinNode*)prototype)->offset1) - rn[i].begin();
+    ptrdiff_t rpos2 = find(rn[i].begin(), rn[i].end(), ((LJoinNode*)prototype)->offset2) - rn[i].begin();
 
     if(rpos1 <= rn.size() || rpos1 <= rn.size()){
       if (rpos1 < rpos2)
@@ -84,12 +86,12 @@ void PJoinNode::Initialize(){
       if(vt == VT_INT){
         if((int)lres[i][lpos] == (int)rres[j][rpos]) join = true;
       }else{
-        if((std::string)lres[i][lpos] == (std::string)rres[j][rpos]) join = true;
+        if((string)lres[i][lpos] == (string)rres[j][rpos]) join = true;
       }
 
       if (join != true) continue;
 
-      std::vector<Value> tmp;
+      vector<Value> tmp;
       for (int k = 0; k < ln.size(); k++){
         if(k != lpos){
             tmp.push_back(lres[i][k]);
@@ -112,9 +114,9 @@ void PJoinNode::Initialize(){
 
 void PJoinNode::Print(int indent){
   for (int i = 0; i < indent; i++){
-    std::cout << " ";
+    cout << " ";
   }
-  std::cout << "NL-JOIN: " << ((LJoinNode*)prototype)->offset1 <<"=" << ((LJoinNode*)prototype)->offset2 << std::endl;
+  cout << "NL-JOIN: " << ((LJoinNode*)prototype)->offset1 <<"=" << ((LJoinNode*)prototype)->offset2 << endl;
   left->Print(indent + 2);
   right->Print(indent + 2);
 }
