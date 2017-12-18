78d975eb6aac148eac3fd0078aba9d15e5af1f9a
Roman Golyshev <anstreth@gmail.com>
2017-12-18 04:10:16 +0300

    refactor histogram a little

--- dbmse/engine/interface/histogram/histogram.cpp
+++ dbmse/engine/interface/histogram/histogram.cpp
@@ -1,11 +1,34 @@
+#include <assert.h>
 #include "histogram.h"
 
 using namespace std;
 
 void test_histograms() {
-    vector<int> data = {10, 5, 16, 94, 33, 82, 11, 45};
-    vector<int> sortedData = data, sort(begin(data), end(data));
+    vector<int> data = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
     Histogram<int> h(data);
+
+    assert((h.CountBelowValue(5) == std::make_pair<size_t, size_t>(0, 1)));
+    assert((h.CountBelowValue(10) == std::make_pair<size_t, size_t>(1, 2)));
+    assert((h.CountBelowValue(15) == std::make_pair<size_t, size_t>(1, 2)));
+    assert((h.CountBelowValue(20) == std::make_pair<size_t, size_t>(2, 3)));
+    assert((h.CountBelowValue(25) == std::make_pair<size_t, size_t>(2, 3)));
+    assert((h.CountBelowValue(30) == std::make_pair<size_t, size_t>(3, 4)));
+    assert((h.CountBelowValue(35) == std::make_pair<size_t, size_t>(3, 4)));
+    assert((h.CountBelowValue(40) == std::make_pair<size_t, size_t>(4, 5)));
+    assert((h.CountBelowValue(45) == std::make_pair<size_t, size_t>(4, 5)));
+    assert((h.CountBelowValue(50) == std::make_pair<size_t, size_t>(5, 6)));
+    assert((h.CountBelowValue(55) == std::make_pair<size_t, size_t>(5, 6)));
+    assert((h.CountBelowValue(60) == std::make_pair<size_t, size_t>(6, 7)));
+    assert((h.CountBelowValue(65) == std::make_pair<size_t, size_t>(6, 7)));
+    assert((h.CountBelowValue(70) == std::make_pair<size_t, size_t>(7, 8)));
+    assert((h.CountBelowValue(75) == std::make_pair<size_t, size_t>(7, 8)));
+    assert((h.CountBelowValue(80) == std::make_pair<size_t, size_t>(8, 9)));
+    assert((h.CountBelowValue(85) == std::make_pair<size_t, size_t>(8, 9)));
+    assert((h.CountBelowValue(90) == std::make_pair<size_t, size_t>(9, 10)));
+    assert((h.CountBelowValue(95) == std::make_pair<size_t, size_t>(9, 10)));
+    assert((h.CountBelowValue(100) == std::make_pair<size_t, size_t>(10, 10)));
+    assert((h.CountBelowValue(105) == std::make_pair<size_t, size_t>(10, 10)));
+    assert((h.CountBelowValue(110) == std::make_pair<size_t, size_t>(10, 10)));
 }
 
 int main() {
--- dbmse/engine/interface/histogram/histogram.h
+++ dbmse/engine/interface/histogram/histogram.h
@@ -3,32 +3,45 @@
 #include <map>
 #include <algorithm>
 #include <functional>
+#include <iostream>
 
 template<class T>
 struct Histogram {
     explicit Histogram(std::vector<T> data,
-                       std::function<bool(const T &, const T &)> ordering = std::less<T>(),
+                       std::function<bool(const T &, const T &)> ascending_ordering = std::less_equal<T>(),
                        size_t number_of_buckets = 10);
 
+    std::pair<size_t, size_t> CountBelowValue(const T &val);
+
 private:
-    struct Bucket : private std::vector<T> {
-        using std::vector<T>::push_back;
-        using std::vector<T>::empty;
+
+    struct BucketInfo {
+        T val;
+        size_t count;
     };
 
     size_t total_records;
-    std::vector<Bucket> buckets;
+    std::function<bool(const T &, const T &)> ordering;
+    std::vector<BucketInfo> bucket_infos;
 };
 
 
 template<class T>
-Histogram<T>::Histogram(std::vector<T> data, std::function<bool(const T &, const T &)> ordering,
+Histogram<T>::Histogram(std::vector<T> data, std::function<bool(const T &, const T &)> ascending_ordering,
                         size_t number_of_buckets)
-    : total_records(data.size())
-    , buckets(number_of_buckets, Bucket()) {
+    : total_records(data.size()), ordering(ascending_ordering) {
+
+    struct Bucket : private std::vector<T> {
+        using std::vector<T>::push_back;
+        using std::vector<T>::empty;
+        using std::vector<T>::size;
+        using std::vector<T>::back;
+    };
+
+    std::vector<Bucket> buckets(number_of_buckets, Bucket());
 
     if (number_of_buckets == 0) {
-        throw std::runtime_error("Cannot have a histogram with zero buckets!");
+        throw std::runtime_error("Cannot have a histogram with zero bucket_infos!");
     }
 
     std::sort(std::begin(data), std::end(data), ordering);
@@ -38,4 +51,38 @@ Histogram<T>::Histogram(std::vector<T> data, std::function<bool(const T &, const
         auto bucket_number = static_cast<size_t> ((i + 1) / records_per_bucket) - 1;
         buckets.at(bucket_number).push_back(data.at(i));
     }
+
+    auto to_remove_from = std::remove_if(std::begin(buckets), std::end(buckets), [](Bucket &b) { return b.empty(); });
+    buckets.erase(to_remove_from, buckets.end());
+
+    for (Bucket &b: buckets) {
+        size_t previous_count = b.size();
+
+        if (!bucket_infos.empty()) {
+            previous_count += bucket_infos.back().count;
+        }
+
+        BucketInfo info = {b.back(), previous_count};
+        bucket_infos.push_back(info);
+    }
+}
+
+template<class T>
+std::pair<size_t, size_t> Histogram<T>::CountBelowValue(const T &val) {
+
+
+    auto first_greater = std::find_if(std::begin(bucket_infos), std::end(bucket_infos), [&](BucketInfo& b) {
+        return !ordering(b.val, val);
+    });
+
+    if (first_greater == std::begin(bucket_infos)) {
+        return std::make_pair(0, first_greater->count);
+    }
+
+    if (first_greater == std::end(bucket_infos)) {
+        return std::make_pair(total_records, total_records);
+    }
+
+    return std::make_pair((first_greater - 1)->count, first_greater->count);
+
 }
